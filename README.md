65abc94e9c30cb78bb10744af8e415f102446bfb
jonathan Goncalves <jonathangoncalves514@gmail.com>
2026-03-20 17:47:44 -0300

    versao inicial(app.py e README.md)

---
+++ README.md
@@ -0,0 +1,20 @@
+#Sobre o projeto
+Projeto com objetivo de calcular o consumo mensal de aparelhos eletronicos e estimar o custo com base no uso diário.
+
+#Tecnologias utilizadas
+Python
+Vs CODE
+GitHub
+
+#Formula utilizada:
+consumo mensal=potencia*horasdia
+
+#Cálculo de custo:
+custo=consumomensal*0.75
+
+#como funciona o programa
+o programa solicita:
+.Nome do aparelho
+.potência em watts do aparelho
+.Tempo médio de uso diário em horas
+
---
+++ app.py
@@ -0,0 +1,21 @@
+#programa para calculo de consumo de energia
+#entrada
+
+aparelho=input("Olá,qual o nome do aparelho?" )
+potencia=float(input("informe a potência do aparelho em watts(w): "))
+horasdia=float(input("Qual o tempo médio de uso diário em horas do aparelho? "))
+
+#processamento
+#calculo
+
+consumomensal=(potencia* horasdia* 30) / 1000
+
+#valor do kwh
+valor_kwh=0.75
+
+#cálculo do custo
+custo=consumomensal* valor_kwh 
+
+print("aparelho=",aparelho)
+print("consumo estimado é de", round(consumomensal),"kwh/mês")
+print("custo estimado e de R$", round(custo,2))
